44e42dc79766fc22b80f0d638c9615a05300eabb
“Danielle <ramirezdanielle88@gmail.com>
2023-12-27 16:19:58 -0800

    shotgun update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from object_render import *
 from sprite_object import *
 from object_handler import *
 from weapon import *
+from sound import *
 
 
 class Game:
@@ -26,6 +27,7 @@ class Game:
         self.raycasting = RayCasting(self)
         self.object_handler = ObjectHandler(self)
         self.weapon = Weapon(self)
+        self.sound = Sound(self)
 
     def update(self):
         self.player.update()
@@ -48,6 +50,7 @@ class Game:
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            self.player.single_fire_event(event)
 
     def run(self):
         while True:
